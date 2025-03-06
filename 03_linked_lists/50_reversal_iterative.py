c5e7e0e00f0ec6a56b80b648d9ed6a389a9fe721
Philippe BAUCOUR <philippe.baucour@gmail.com>
2025-03-06 08:42:39 +0100

    start linked list

---
+++ 03_linked_lists/50_reversal_iterative.py
@@ -0,0 +1,86 @@
+# Reverse a linked list iteratively
+
+# O(n), O(1)
+# En complexité on est en O(n) car on parcourt la liste une seule fois
+# En espace, O(1) car l'inversion se fait en place
+
+from __future__ import annotations
+
+
+class List_Node:
+    def __init__(self, val: int, next: List_Node | None):
+        self.val = val
+        self.next = next
+
+
+def reverse_list(head: List_Node | None) -> List_Node | None:
+    curr_node: List_Node | None = head
+    prev_node: List_Node | None = None
+
+    while curr_node:
+        # 2
+        next_node = curr_node.next
+
+        # 1
+        # This is the first line we want to write BUT we need to store the next node before changing the pointer
+        # This explain the line above
+        curr_node.next = prev_node
+
+        # 3
+        # Shifiting the pointers
+        prev_node = curr_node
+        curr_node = next_node
+
+    return prev_node
+
+
+if __name__ == "__main__":
+    head: List_Node | None = List_Node(1, List_Node(2, List_Node(3, List_Node(4, None))))
+    head = reverse_list(head)
+    while head:
+        print(head.val)
+        head = head.next
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+# class List_Node:
+#     def __init__(self, val, next):
+#         self.val = val
+#         self.next = next
+
+
+# def reverse_list(head):
+#     curr_node = head
+#     prev_node = None
+
+#     while curr_node:
+#         # 2
+#         next_node = curr_node.next
+
+#         # 1
+#         # This is the first line we want to write BUT we need to store the next node before changing the pointer
+#         # This explains the line above
+#         curr_node.next = prev_node
+
+#         # 3
+#         # Shifting the pointers
+#         prev_node = curr_node
+#         curr_node = next_node
+
+#     return prev_node
+
+
+# if __name__ == "__main__":
+#     head: List_Node | None = List_Node(1, List_Node(2, List_Node(3, List_Node(4, None))))
+#     head = reverse_list(head)
+#     while head:
+#         print(head.val)
+#         head = head.next
---
+++ 03_linked_lists/54_reversal_recursive.py
@@ -0,0 +1,42 @@
+# Reverse a linked list recursively
+
+# O(n), O(n)
+# En complexité on est en O(n) car on parcourt la liste une seule fois
+# En espace, O(n) car on empile les élément sur la stack du PC
+from __future__ import annotations
+
+
+class List_Node:
+    def __init__(self, val: int, next: List_Node | None):
+        self.val = val
+        self.next = next
+
+
+def reverse_list(head: List_Node | None) -> List_Node | None:
+    # Base cases
+    # Si la liste est vide ou si elle ne contient qu'un seul élément y a rien à faire
+    if not head or not head.next:
+        return head
+    # 1
+    # On veut commencer par régler le sous problème
+    # Donc on inverse la list dont la tête est head.next
+    new_head = reverse_list(head.next)
+
+    # 2
+    # Il faut inverser les pointeurs
+    # Bien voir que head.next c'est (2) head.next.next c'est (2).next
+    # C'est bien lui qu'on veut faire pointer sur head (et plus sur (3))
+    head.next.next = head
+    # head va se retrouver en fin de liste donc head.next doit pointer sur None
+    head.next = None
+
+    # Bien voir que c'est le new-head retourné par reverse_list(head.next)
+    return new_head
+
+
+if __name__ == "__main__":
+    head: List_Node | None = List_Node(1, List_Node(2, List_Node(3, List_Node(4, None))))
+    head = reverse_list(head)
+    while head:
+        print(head.val)
+        head = head.next
